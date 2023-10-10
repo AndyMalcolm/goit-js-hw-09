2d348c194f1cf5154b1793f731fcd173caaa2210
AndyMalcolm <ayhrunin.fitu21@kubg.edu.ua>
2023-10-10 13:25:48 +0300

    nice

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,3 +1,5 @@
 function getRandomHexColor() {
     return `#${Math.floor(Math.random() * 16777215).toString(16).padStart(6, 0)}`;
   }
+const buttonStart = document.querySelector('[data-start]')
+const buttonStop = document.querySelector('[data-stop]')
