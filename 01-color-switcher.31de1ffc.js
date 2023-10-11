d0391eb9a8a17f8f8c9083bb2f379021bf153d08
AndyMalcolm <125152622+AndyMalcolm@users.noreply.github.com>
2023-10-11 09:09:10 +0000

    Deploying to gh-pages from @ AndyMalcolm/goit-js-hw-09@d11cd1cff3f7230c469a041e14a4d3c6fbef8b82 🚀

--- 01-color-switcher.31de1ffc.js
+++
@@ -1,2 +0,0 @@
-document.querySelector("[data-start]"),document.querySelector("[data-stop]"),document.body;setTimeout();
-//# sourceMappingURL=01-color-switcher.31de1ffc.js.map
--- 01-color-switcher.6c69f469.js
+++
@@ -1,2 +0,0 @@
-!function(){document.querySelector("[data-start]"),document.querySelector("[data-stop]"),document.body;setTimeout()}();
-//# sourceMappingURL=01-color-switcher.6c69f469.js.map
