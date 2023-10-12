545bb256ef679858f309690cf30ee12b01df4e41
AndyMalcolm <ayhrunin.fitu21@kubg.edu.ua>
2023-10-12 11:53:49 +0300

    nice

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,22 +1,32 @@
 function getRandomHexColor() {
-    return `#${Math.floor(Math.random() * 16777215).toString(16).padStart(6, 0)}`;
-  }
-const buttonStart = document.querySelector('[data-start]');
-const buttonStop = document.querySelector('[data-stop]');
+  return `#${Math.floor(Math.random() * 16777215).toString(16).padStart(6, '0')}`;
+}
+
+const startButton = document.querySelector('[data-start]');
+const stopButton = document.querySelector('[data-stop]');
 const body = document.body;
+
 let intervalId;
+let isChangingColor = false;
+
 function changeBackgroundColor() {
   body.style.backgroundColor = getRandomHexColor();
 }
-function onStartButtonClick() {
+
+startButton.addEventListener('click', () => {
+  if (isChangingColor) return;
+
+  intervalId = setInterval(changeBackgroundColor, 1000);
+  isChangingColor = true;
+
   startButton.disabled = true;
   stopButton.disabled = false;
-  intervalId = setInterval(changeBackgroundColor, 1000);
-}
-function onStopButtonClick() {
+});
+
+stopButton.addEventListener('click', () => {
+  clearInterval(intervalId);
+  isChangingColor = false;
+
   startButton.disabled = false;
   stopButton.disabled = true;
-  clearInterval(intervalId);
-}
-startButton.addEventListener('click', onStartButtonClick);
-stopButton.addEventListener('click', onStopButtonClick);
+});
