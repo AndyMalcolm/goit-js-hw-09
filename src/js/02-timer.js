537c7c6799bc54ae91a0bb003783b50052080a87
AndyMalcolm <ayhrunin.fitu21@kubg.edu.ua>
2023-10-17 12:56:49 +0300

    50\50

--- src/js/02-timer.js
+++ src/js/02-timer.js
@@ -1,51 +1,70 @@
+
 import flatpickr from "flatpickr";
 import "flatpickr/dist/flatpickr.min.css";
-function addLeadingZero(value) {
-    return value.toString().padStart(2, '0');
-  }
 
-  function updateTimer(days, hours, minutes, seconds) {
-    document.querySelector('[data-days]').textContent = addLeadingZero(days);
-    document.querySelector('[data-hours]').textContent = addLeadingZero(hours);
-    document.querySelector('[data-minutes]').textContent = addLeadingZero(minutes);
-    document.querySelector('[data-seconds]').textContent = addLeadingZero(seconds);
-  }
-  function onDateSelected(selectedDates) {
-    const selectedDate = selectedDates[0];
-    const currentDate = new Date();
-    if (selectedDate <= currentDate) {
-      alert("Please choose a date in the future");
-      document.querySelector('[data-start]').disabled = true;
-    } else {
-      document.querySelector('[data-start]').disabled = false;
-    }
-  }
-  function startCountdown() {
-    const selectedDate = flatpickr.parseDate(document.querySelector('#datetime-picker').value);
-    const currentDate = new Date();
-    const timeDifference = selectedDate - currentDate;
-  
-    if (timeDifference <= 0) {
-      alert("Please choose a date in the future");
-      return;
-    }
-  
-    const interval = setInterval(function () {
-      const time = convertMs(selectedDate - new Date());
-      updateTimer(time.days, time.hours, time.minutes, time.seconds);
-  
-      if (timeDifference <= 0) {
-        clearInterval(interval);
-      }
-    }, 1000);
-  }
-  const options = {
-    enableTime: true,
-    time_24hr: true,
-    defaultDate: new Date(),
-    minuteIncrement: 1,
-    onClose: onDateSelected,
+function getTimeRemaining(endtime) {
+  const total = Date.parse(endtime) - Date.now();
+  const seconds = Math.floor((total / 1000) % 60);
+  const minutes = Math.floor((total / 1000 / 60) % 60);
+  const hours = Math.floor((total / (1000 * 60 * 60)) % 24);
+  const days = Math.floor(total / (1000 * 60 * 60 * 24));
+
+  return {
+    total,
+    days,
+    hours,
+    minutes,
+    seconds
   };
-  
-  flatpickr('#datetime-picker', options);
-  document.querySelector('[data-start]').addEventListener('click', startCountdown);
+}
+
+function addLeadingZero(value) {
+  return value < 10 ? `0${value}` : value;
+}
+
+const datetimePicker = document.getElementById("datetime-picker");
+const startButton = document.querySelector("[data-start]");
+const daysElement = document.querySelector("[data-days]");
+const hoursElement = document.querySelector("[data-hours]");
+const minutesElement = document.querySelector("[data-minutes]");
+const secondsElement = document.querySelector("[data-seconds]");
+
+const options = {
+  enableTime: true,
+  time_24hr: true,
+  minuteIncrement: 1,
+  onClose: function (selectedDates) {
+    const selectedDate = selectedDates[0];
+
+    if (selectedDate <= new Date()) {
+      alert("Please choose a date in the future");
+      startButton.disabled = true;
+    } else {
+      startButton.disabled = false;
+    }
+  }
+};
+
+flatpickr(datetimePicker, options);
+
+startButton.addEventListener("click", function () {
+  const selectedDate = flatpickr.parseDate(datetimePicker.value);
+  const countdown = setInterval(updateCountdown, 1000);
+
+  function updateCountdown() {
+    const timeRemaining = getTimeRemaining(selectedDate);
+
+    if (timeRemaining.total <= 0) {
+      clearInterval(countdown);
+      daysElement.textContent = "00";
+      hoursElement.textContent = "00";
+      minutesElement.textContent = "00";
+      secondsElement.textContent = "00";
+    } else {
+      daysElement.textContent = addLeadingZero(timeRemaining.days);
+      hoursElement.textContent = addLeadingZero(timeRemaining.hours);
+      minutesElement.textContent = addLeadingZero(timeRemaining.minutes);
+      secondsElement.textContent = addLeadingZero(timeRemaining.seconds);
+    }
+  }
+});
