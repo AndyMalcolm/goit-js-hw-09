17b8c011a09bd2b17f236981b4e07fdbf43f3dac
AndyMalcolm <ayhrunin.fitu21@kubg.edu.ua>
2023-10-18 15:16:03 +0300

    tak?

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,8 +1,11 @@
 function createPromise(position, delay) {
+  return new Promise((resolve, reject) => {
   const shouldResolve = Math.random() > 0.3;
   if (shouldResolve) {
-    // Fulfill
+    // Fulfill 
+    resolve({ position, delay });
   } else {
     // Reject
-  }
+    reject({ position, delay });
+  }}, delay);
 }
