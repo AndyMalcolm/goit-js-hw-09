8c1729c484907b9933bbe315e9fb4a6546c4492a
AndyMalcolm <ayhrunin.fitu21@kubg.edu.ua>
2023-10-21 21:09:52 +0300

    hm

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,11 +1,29 @@
 function createPromise(position, delay) {
   return new Promise((resolve, reject) => {
-  const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
-    // Fulfill 
-    resolve({ position, delay });
-  } else {
-    // Reject
-    reject({ position, delay });
-  }}, delay);
+    const shouldResolve = Math.random() > 0.3;
+    setTimeout(() => {
+      if (shouldResolve) {
+        resolve({ position, delay });
+      } else {
+        reject({ position, delay });
+      }
+    }, delay);
+  });
 }
+
+function handleFormSubmit(event) {
+  event.preventDefault();
+  const position = 0;
+  const delay = 0;
+
+  createPromise(position, delay)
+    .then((result) => {
+      console.log('Promise resolved:', result);
+    })
+    .catch((error) => {
+      console.error('Promise rejected:', error);
+    });
+}
+
+const form = document.querySelector('form');
+form.addEventListener('submit', handleFormSubmit);
