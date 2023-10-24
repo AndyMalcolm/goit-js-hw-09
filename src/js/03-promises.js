b6baa01c9daff1adac3cdd3b9ba4f83d3921bd82
AndyMalcolm <ayhrunin.fitu21@kubg.edu.ua>
2023-10-24 23:58:54 +0300

    bruh

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -15,20 +15,5 @@ function handleFormSubmit(event) {
     delay = Number(delay) + Number(step);
   }
 }
-
-function handleFormSubmit(event) {
-  event.preventDefault();
-  const delay = form.elements.delay.value;
-  const step = form.elements.step.value;
-
-  createPromise(step, delay)
-    .then((result) => {
-      console.log('Promise resolved:', result);
-    })
-    .catch((error) => {
-      console.error('Promise rejected:', error);
-    });
-}
-
 const form = document.querySelector('form');
 form.addEventListener('submit', handleFormSubmit);
